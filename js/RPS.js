88280b8d7efa9dc30988165d5220f045e3b62371
supersavvy <gupta.savvy@gmail.com>
2018-02-05 16:56:07 -0800

    Update RPS.js

--- js/RPS.js
+++ js/RPS.js
@@ -1,47 +1,80 @@
-var player = prompt("Choose one!");
-var comp = Math.random();
-if (comp < 0.34) {
-	comp = "rock";
-} else if (comp < 0.67) {
-	comp = "paper";
-} else {
-	comp = "scissors";
-}
-document.write("Computer: " + comp);
-var result = play(player, computer);
-document.write("Results: " + result);
+var playerW = 0;
+var compW = 0;
+var ties = 0;
+var pChoice;
+var message;
+var scoreboard;
 
-var play = function(player, computer) {
-	print(comp);
-	if (player == "gun") {
-		return "Incredible! you win!"
-	}
-	if (player == computer) {
-		return "Tie!";
-	}
-	if (player == "rock") {
-		if (computer == "paper") {
-			return "you lose!";
-		}
-		if (computer == "scissors") {
-			return "you win!";
-		}
-	}
-	if (player == "paper") {
-		if (computer == "scissors") {
-			return "you lose!";
-		}
-		if (computer == "rock") {
-			return "you win!";
-		}
-	}
-	if (player == "scissors") {
-		if (computer == "paper") {
-			return "you win!";
-		}
-		if (computer == "rock") {
-			return "you lose!";
-		}
-	}
-}
+$('.rock').click(function() {
+	pChoice = 1;
+	var comp = Math.floor(Math.random() * 3 + 1);
 
+	if (comp == pChoice) {
+		message.html('').append("Tie! Computer chose rock.");
+		ties++;
+		$('#ties').html('').append(ties);
+	});
+	else if (comp == 2) {
+		message.html('').append("You Lost! Computer chose paper.");
+		compW++;
+		$('#losses').html('').append(compW);
+	});
+	else {
+		message.html('').append("You Won! Computer chose scissors.");
+		playerW++;
+		$('#wins').html('').append(playerW);
+	});
+});
+
+$('.paper').click(function() {
+	pChoice = 2;
+	var comp = Math.floor(Math.random() * 3 + 1);
+
+	if (comp == pChoice) {
+		message.html('').append("Tie! Computer chose paper.");
+		ties++;
+		$('#ties').html('').append(ties);
+	});
+	else if (comp == 3) {
+		message.html('').append("You Lost! Computer chose scissors.");
+		compW++;
+		$('#losses').html('').append(compW);
+	});
+	else {
+		message.html('').append("You Won! Computer chose rock.");
+		playerW++;
+		$('#wins').html('').append(playerW);
+	});
+});
+
+$('.scissors').click(function() {
+	pChoice = 3;
+	var comp = Math.floor(Math.random() * 3 + 1);
+
+	if (comp == pChoice) {
+		message.html('').append("Tie! Computer chose scissors.");
+		ties++;
+		$('#ties').html('').append(ties);
+	});
+	else if (comp == 1) {
+		message.html('').append("You Lost! Computer chose rock.");
+		compW++;
+		$('#losses').html('').append(compW);
+	});
+	else {
+		message.html('').append("You Won! Computer chose paper.");
+		playerW++;
+		$('#wins').html('').append(playerW);
+	});
+});
+
+
+
+$('#reset').click(function() {
+  playerW = 0;
+  $('#wins').html(playerW);
+  ties = 0;
+  $('#ties').html(ties);
+  compW = 0;
+  $('#losses').html(compW);
+});
